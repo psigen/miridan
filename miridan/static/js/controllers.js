440b2ab0cccfa51e85e0a0235756996878dea359
PyryM <pkmatikainen@gmail.com>
2014-07-19 03:42:07 -0400

    list action arguments in select

--- miridan/static/js/controllers.js
+++ miridan/static/js/controllers.js
@@ -34,24 +34,30 @@ miridanApp.controller('InventoryListController', function ($scope, $rootScope) {
 
 miridanApp.controller('InputController', function ($scope, $rootScope) {
   $scope.targets = [];
-  $scope.possibleActions = [{'name': 'explode'}, {'name': 'hug'}];
+  $scope.possibleActions = [{'name': 'explode', 'args': ['target', 'explosive']}, 
+                            {'name': 'hug', 'args': ['target_player']}];
   $scope.selectedAction = $scope.possibleActions[0];
 
   $scope.$on('selectTarget', function(event, newtarget) {
     $scope.targets.push(newtarget);
   });
 
+  $scope.formatActionName = function(action) {
+    return action.name + ":[" + action.args.join("][") + "]";
+  }
+
   $scope.doaction = function() {
     var actionstr = $scope.selectedAction.name;
+    var aargs = $scope.selectedAction.args;
     for(var i = 0; i < $scope.targets.length; ++i) {
-      actionstr += (" [" + $scope.targets[i].name + "]");
+      actionstr += (" [" + (aargs[i] || "?") + ":" + $scope.targets[i].name + "]");
     }
-    $rootScope.$broadcast('addLogs', [{'text': actionstr}]);
+    $rootScope.$broadcast('addLogs', [{'message': actionstr}]);
   };
 });
 
 miridanApp.controller('LogController', function ($scope) {
-  $scope.logs = [{'text': 'WELCOM TO MIRIDAN!!!'}];
+  $scope.logs = [{'message': 'WELCOM TO MIRIDAN!!!'}];
 
   $scope.$on('addLogs', function(event, newlogs) {
     for(var i = 0; i < newlogs.length; ++i) {
